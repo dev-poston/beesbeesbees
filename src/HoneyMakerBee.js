f3ef21f3664a68b3398446eeb1095d09cc4d71a7
Devon Poston <dev.poston@gmail.com>
2021-01-11 17:42:56 -0800

    Complete HoneyMakerBee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,8 +1,18 @@
 var HoneyMakerBee = function() {
   Bee.call(this);
-
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
-HoneyMakerBee.prototype.constructor = Object.create(Bee.prototype);
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
 
 HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot ++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot --;
+};
